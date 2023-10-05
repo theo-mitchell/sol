aefd9cd1bb600f4b84f7afab8692e12880f12f6c
Theo <theo.development@pm.me>
2023-10-04 18:44:56 -0600

    chore: created grunt-connect server

---
+++ sol_server/Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          keepalive: true,
+        },
+        base: ".",
+      },
+    },
+
+    uglify: {
+      options: {
+        banner:
+          '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+      },
+      build: {
+        src: "src/<%= pkg.name %>.js",
+        dest: "build/<%= pkg.name %>.min.js",
+      },
+    },
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-connect");
+
+  //default task(s)
+  grunt.registerTask("default", ["uglify"]);
+};
